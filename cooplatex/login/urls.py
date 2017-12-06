ffb5cd3b596e76a0537b76a15dcf491147e51c4b
Carlos Gonzalez Betancort <cg14823@my.bristol.ac.uk>
2017-12-06 10:25:51 +0000

    email verification working

--- cooplatex/login/urls.py
+++ cooplatex/login/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^signin/', views.sign_in, name='signIn'),
     url(r'^signup/', views.register, name='register'),
     url(r'^verify/(?P<userID>[0-9]+)/(?P<verifyToken>[\w]{32})', views.verifyUser, name='verifyUser'),
+    url(r'^pleaseverify/', views.pleaseVerify, name='pleaseVerify'),
 ]
